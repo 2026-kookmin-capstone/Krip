9a64eff1291b10b9f3340b623a6de4e27a93ddb1
hyeonsang010716 <kyr76789@gmail.com>
2026-04-16 22:47:39 +0900

    feat: user_travel_style_repository -> user_travel_style 로 이름 변경

--- backend/app/domain/auth/service/register.py
+++ backend/app/domain/auth/service/register.py
@@ -2,7 +2,7 @@ from typing import List
 
 from app.domain.auth.repository.user_detail_inform import UserDetailInformRepository
 from app.domain.auth.repository.user import UserRepository
-from app.domain.auth.repository.user_travel_style_repository import UserTravelStyleRepository
+from app.domain.auth.repository.user_travel_style import UserTravelStyleRepository
 from app.domain.auth.model.user_travel_style import UserTravelStyle, TravelStyle
 from app.domain.auth.model.user_detail_inform import UserDetailInform, Gender
 from app.database.session import UnitOfWork, transactional
